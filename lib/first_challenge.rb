3e385747d3509fb5957f74ae41e27bc177f2065d
Carla Stickler <carlastickler@gmail.com>
2019-04-19 19:43:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
         value.each do |flavor|
           if value.include? "strawberry"
             new_flavors = value.delete("strawberry")
-                end
+                
               end
           end
         end
